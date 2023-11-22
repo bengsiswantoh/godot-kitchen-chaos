a6eeec3d9ea63750bdc5403bd5418449b91095aa
Bengsiswanto <bengsiswantoh@gmail.com>
2023-11-22 15:43:14 +0700

    change gravity

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,13 +2,26 @@ extends CharacterBody3D
 
 @export_subgroup("Properties")
 # How fast the player moves
-@export var speed = 150
+@export var movement_speed = 150
 # The downward acceleration when in the air
-@export var fall_acceleration = 150
+@export var gravity_speed = 150
 
-var target_velocity = Vector3.ZERO
+var movement_velocity = Vector3.ZERO
+var y_velocity = 0
 
 func _physics_process(delta):
+	handle_controls(delta)
+	handle_gravity(delta)
+	
+	var applied_velocity: Vector3
+	
+	applied_velocity = velocity.lerp(movement_velocity, delta * 10)
+	applied_velocity.y = -y_velocity
+
+	velocity = applied_velocity
+	move_and_slide()
+
+func handle_controls(delta):
 	var input = Vector3.ZERO
 
 	input.x = Input.get_axis("move_left", "move_right")
@@ -17,16 +30,12 @@ func _physics_process(delta):
 	if input != Vector3.ZERO:
 		input = input.normalized()
 		$Pivot.look_at(position + input, Vector3.UP)
+		
+	movement_velocity = input * movement_speed * delta
 
-	# Ground Velocity
-	target_velocity = input * speed
-
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - fall_acceleration
+func handle_gravity(delta):
+	y_velocity += gravity_speed * delta
 	
-	target_velocity *= delta
-
-	# Moving the Character
-	velocity = target_velocity
-	move_and_slide()
+	if y_velocity > 0 and is_on_floor():
+		y_velocity = 0
+		
