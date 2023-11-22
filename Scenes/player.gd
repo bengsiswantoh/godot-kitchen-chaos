9306600d9090be1fe72b6b8741f6f466a600e620
Bengsiswanto <bengsiswantoh@gmail.com>
2023-11-22 14:48:34 +0700

    change floor to plane

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,35 +1,31 @@
 extends CharacterBody3D
 
-# How fast the player moves in meters per second.
-@export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
-@export var fall_acceleration = 75
+@export_subgroup("Properties")
+# How fast the player moves
+@export var speed = 150
+# The downward acceleration when in the air
+@export var fall_acceleration = 150
 
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
-	var direction = Vector3.ZERO
+	var input = Vector3.ZERO
 
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
 
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		$Pivot.look_at(position + direction, Vector3.UP)
+	if input != Vector3.ZERO:
+		input = input.normalized()
+		$Pivot.look_at(position + input, Vector3.UP)
 
 	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	target_velocity = input * speed
 
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+		target_velocity.y = target_velocity.y - fall_acceleration
+	
+	target_velocity *= delta
 
 	# Moving the Character
 	velocity = target_velocity
