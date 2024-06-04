e38f081e883c9b56485448fdda9ac59b89069a6e
bengsiswanto <bengsiswantoh@gmail.com>
2024-06-04 22:07:24 +0700

    add character script

---
+++ Scripts/Objects/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+
+@export var _speed: float = 5.0
+@export var _visual: Node3D
+
+
+var _direction: Vector3
+var _facing_angle: float
+
+
+func _input(_event: InputEvent) -> void:
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	_direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+
+func _physics_process(_delta: float) -> void:
+	if _direction:
+		velocity.x = _direction.x * _speed
+		velocity.z = _direction.z * _speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, _speed)
+		velocity.z = move_toward(velocity.z, 0, _speed)
+
+	move_and_slide()
+	rotate_visual()
+
+
+func rotate_visual() -> void:
+	var is_moving: bool = _direction.x != 0 or _direction.z != 0
+		
+	if is_moving:
+		_facing_angle = Vector2(_direction.z, _direction.x).angle()
+		_visual.rotation.y = lerp_angle(_visual.rotation.y, _facing_angle, 0.5)
