687a556196e554b6f34fe51dc787ddcac6c6efb3
bengsiswanto <bengsiswantoh@gmail.com>
2024-06-07 23:55:42 +0700

    container counter cheese

--- Scripts/Counters/clear_counter.gd
+++ Scripts/Counters/clear_counter.gd
@@ -5,5 +5,10 @@ extends BaseCounter
 @export var _kitchen_object_resource: KitchenObjectResource
 
 
-func interact(_player: Player) -> void:
-	pass
+func interact(player: Player) -> void:
+	if not has_kitchen_object():
+		if player.has_kitchen_object():
+			player.get_kitchen_object().set_kitchen_object_parent(self)
+	else:
+		if not player.has_kitchen_object():
+			get_kitchen_object().set_kitchen_object_parent(player)
--- Scripts/Counters/container_counter.gd
+++ Scripts/Counters/container_counter.gd
@@ -6,6 +6,6 @@ extends BaseCounter
 
 
 func interact(player: Player) -> void:
-	var new_kitchen_object = _kitchen_object_resource.create_scene() as KitchenObject
-	new_kitchen_object.set_kitchen_object_parent(player)
-	
+	if not player.has_kitchen_object():
+		var new_kitchen_object = _kitchen_object_resource.create_scene() as KitchenObject
+		new_kitchen_object.set_kitchen_object_parent(player)
